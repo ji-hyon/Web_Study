20b7b8b97bb6460d023d24b59b80e4546469c41e
ji-hyon <gyg03134@gmail.com>
2023-05-07 22:37:37 +0900

    web_study

---
+++ Vue/practice/part3_CLI_Router/problem2/src/views/TheLotto.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>로또</h1>
+    <button @click="getNums">Get Lucky Numbers</button>
+    <p>{{nums}}</p>
+  </div>
+</template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js"></script>
+<script>
+export default {
+  name: 'TheLotto',
+  data: function() {
+    return{
+      nums : [],
+    }
+  },
+  methods: {
+    getNums: function() {
+      const numbers = _.range(1, 46)
+      this.nums = _.sampleSize(numbers, this.$route.params.lottoNum)
+      // this.nums = _.sortBy(this.sampleNums)
+    }
+  }
+}
+</script>
---
+++ Vue/practice/part3_CLI_Router/problem2/src/views/TheLunch.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <div>
+    <h1>점심메뉴</h1>
+    <button @click="pickMenu">Pick Lunch</button>
+    <p>오늘의 추천 : {{menu}}</p>
+    <br>
+    <h2>로또를 뽑아보자</h2>
+    <button @click="pickNum">Pick Lotto</button>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'TheLunch',
+//   components: {
+//     HelloWorld
+//   }
+  data: function() {
+    return {
+      lunch: ['더고메', '육수고집', '차이나호', '소담상', '속이찬새참'],
+      menu: null,
+    }
+  },
+  methods: {
+    pickMenu: function() {
+      this.menu = _.sample(this.lunch)
+    },
+    pickNum: function() {
+      this.$router.push({name: 'TheLotto', params:{lottoNum:6}})
+    }
+  }
+}
+</script>
