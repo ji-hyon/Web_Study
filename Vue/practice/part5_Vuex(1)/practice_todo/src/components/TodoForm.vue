9f4c9099cdb19cf27dbe53ee05a8e9993b483153
ji-hyon <gyg03134@gmail.com>
2023-05-08 21:20:34 +0900

    web_study

---
+++ Vue/practice/part5_Vuex(1)/practice_todo/src/components/TodoForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type="text" v-model.trim="todoTitle" @keyup.enter="createTodo">
+</template>
+
+<script>
+export default {
+    name: 'TodoForm',
+    data() {
+        return {
+            todoTitle: null,
+        }
+    },
+    methods: {
+        createTodo() {
+            // console.log(this.todoTitle);
+            if(this.todoTitle) {
+                this.$store.dispatch('createTodo', this.todoTitle)
+            } else {
+                alert('할일을 입력하세요!')
+            }
+            this.todoTitle = null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
