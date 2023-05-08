9b2aa80c028753aa1e023c897467c74782c147b0
ji-hyon <gyg03134@gmail.com>
2023-05-08 23:17:42 +0900

    web_study

---
+++ Vue/practice/part6_Vuex(2)/task4/App.vue
@@ -0,0 +1,6 @@
+export default {
+  name: 'App',
+  created: function () {
+    this.$store.dispatch('fetchTodoList')
+  },
+}
---
+++ Vue/practice/part6_Vuex(2)/task4/store/index.js
@@ -0,0 +1,11 @@
+// store/index.js
+
+// actions: {
+//     fetchTodoList: async function ({ commit }) {
+//       const requestUrl = 'http://localhost:8000/api/v1/todos/'
+  
+//       const response = await axios.get(requestUrl)
+//       commit('TODO_LIST_SUCCESS', response)
+//     },
+//   }
+
