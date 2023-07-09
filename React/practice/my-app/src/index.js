22c0fc7564a1ed98a0e5370da34528d7611bde6f
ji-hyon <gyg03134@gmail.com>
2023-07-09 23:07:45 +0900

    Event

--- React/practice/my-app/src/index.js
+++ React/practice/my-app/src/index.js
@@ -9,12 +9,13 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import Notification from './chapter_06/Notification';
 import NotificationList from './chapter_06/NotificationList';
-import Cherry from './my_code/Cherry';
+import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cherry />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- React/practice/my-app/src/my_code/Cherry.jsx
+++ React/practice/my-app/src/my_code/Cherry.jsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from "react";
+function MyButton(props) {
+    const handleDelete = (id, event) => {
+        console.log(id, event.target);
+    };
 
-function Cherry(props) {
-    const [count, setCount] = useState(0);
-
-    // useEffect(() => {
-    //     // 브라우저 API를 사용해서 document의 title을 업데이트
-    //     document.title = `You clicked ${count} times`;
-    // });
-    document.title = `You clicked ${count} times`;
-    
     return (
-        <div>
-            <p>총 {count}번 클릭했습니다.</p>
-            <button onClick={() => setCount(count + 1)}>
-                클릭
-            </button>
-        </div>
+        <button onClick={(event) => handleDelete(1, event)}>
+            삭제하기 
+        </button>
     );
 }
-
-export default Cherry
