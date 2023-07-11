adcca085ad3f3cbe615c30027cba1b4249f16bde
ji-hyon <gyg03134@gmail.com>
2023-07-11 23:58:25 +0900

    keys

--- React/practice/my-app/src/my_code/Cherry.jsx
+++ React/practice/my-app/src/my_code/Cherry.jsx
@@ -1,18 +1,7 @@
-const { useState } = require("react");
+function RequestForm(props) {
+    const [value, setValue] = useState('요청사항을 입력하세요.');
 
-function MainPage(props) {
-    const [showWarning, setShowWarning] = useState(false);
-
-    const handleToggleClick = () => {
-        setShowWarning(prevShowWarning => !prevShowWarning);
+    const handleChange = (event) => {
+        setValue(event.target.value);
     }
-
-    return (
-        <div>
-            <WarningBanner warning={showWarning} />
-            <button onClick={handleToggleClick}>
-                {showWarning ? '감추기' : '보이기'}
-            </button>
-        </div>
-    )
 }
