fba26e770e496d570f5f8bc34c7da9c921c70982
ji-hyon <gyg03134@gmail.com>
2024-02-14 15:42:04 +0900

    feat: MemberService 함수에 시간 측정 코드 추가

--- Spring/Spring_Intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Spring/Spring_Intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -31,26 +31,44 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
+
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+    }
+
+    private void validateDuplicateMember(Member member) {
         // 같은 이름이 있는 중복 회원X
 //        Optional<Member> result = memberRepository.findByName(member.getName());
 //        result.ifPresent(m -> {
 //            throw new IllegalStateException("이미 존재하는 회원입니다.");
 //        });
-        // extract Method하여 리팩토링하기 (validateDuplicateMember)
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-
-        memberRepository.save(member);
-        return member.getId();
     }
 
     /**
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     /**
