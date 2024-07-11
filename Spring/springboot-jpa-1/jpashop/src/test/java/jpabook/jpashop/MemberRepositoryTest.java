b2513732e1a53a698646932976150d609ae78ebb
ji-hyon <gyg03134@gmail.com>
2024-07-11 10:58:08 +0900

    JPA테스트: entity, repository, repositoryTest 생성

---
+++ Spring/springboot-jpa-1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 엔티티 매니저를 통한 모든 데이터 변경은 항상 트랜잭션 안에서 이루어져야 함
+                    // 테스트의 경우, 테스트가 끝나면 롤백
+//    @Rollback(false) // 롤백하지 않고 DB에서 확인하고 싶을 때
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
+            // 1차 캐시, 같은 영속성 컨텍스트 안에서는 id 값이 같으면 같은 엔티티로 식별
+        System.out.println("findMember == member: " + (findMember == member)); //
+    }
+
+
+}
